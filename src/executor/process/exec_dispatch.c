6ca5b9e0fe87fa7931ed5c206beea2f67ea2e007
vhacman <hacmanvioricagabriela@gmail.com>
2025-08-25 17:45:31 +0200

    restore

--- src/executor/process/exec_dispatch.c
+++ src/executor/process/exec_dispatch.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_dispatch.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vhacman <vhacman@student.42roma.it>        +#+  +:+       +#+        */
+/*   By: vhacman <vhacman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 19:24:00 by vhacman           #+#    #+#             */
-/*   Updated: 2025/08/25 15:38:41 by vhacman          ###   ########.fr       */
+/*   Updated: 2025/08/24 12:07:15 by vhacman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,6 @@ int	execute_command_type(char **args, t_shell *shell)
 	char	*command_path;
 	int		status;
 
-	if (!args || !args[0])
-		return (127);
 	if (is_builtin(args[0]))
 		return (handle_builtin(args, shell));
 	command_path = find_command_path(args[0], shell);
@@ -66,68 +64,50 @@ int	execute_command_type(char **args, t_shell *shell)
 	return (status);
 }
 
-int	prepare_command(t_token *tokens, t_shell *shell, char ***args)
-{
-	*args = NULL;
-	if (!tokens)
-		return (handle_null_tokens(shell));
-	if (check_for_pipes(tokens))
-		return (handle_pipeline_execution(tokens, shell));
-	*args = prepare_cmd_args(tokens, shell);
-	if (setup_redirection(tokens, *args, shell) == -1)
-	{
-		if (*args)
-			free_args_array(*args);
-		*args = NULL;
-		shell->exit_status = 1;
-		return (1);
-	}
-	if (!*args || !(*args)[0] || (*args)[0][0] == '\0')
-	{
-		restore_redirection(shell);
-		cleanup_per_command(shell);
-		if (*args)
-		{
-			free_args_array(*args);
-			*args = NULL;
-		}
-		shell->exit_status = 0;
-		return (0);
-	}
-	return (-1);
-}
-
+/*
+** execute_command
+**
+** This function executes a full command from a list of tokens.
+** - If no tokens exist, call handle_null_tokens().
+** - If the tokens contain pipes, delegate execution to
+**   handle_pipeline_execution().
+** - Otherwise:
+**     * Build the command arguments with prepare_cmd_args().
+**     * If no args, return 127.
+**     * Setup redirections with setup_redirection().
+**         - If redirection fails, return 1.
+**     * If the command is a builtin:
+**         - If it is "exit", call execute_exit().
+**         - Otherwise, call handle_other_builtins().
+**     * If not a builtin, call handle_external_command().
+** - Free the args array after execution.
+** - Store the status in shell->exit_status.
+** - Return the final status.
+*/
 int	execute_command(t_token *tokens, t_shell *shell)
 {
 	char	**args;
 	int		status;
-	int		prep_result;
 
-	args = NULL;
-	prep_result = prepare_command(tokens, shell, &args);
-	if (prep_result != -1)
-		return (prep_result);
-	if (!args || !args[0])
-	{
-		restore_redirection(shell);
-		cleanup_per_command(shell);
-		shell->exit_status = 127;
+	if (!tokens)
+		return (handle_null_tokens(shell));
+	if (check_for_pipes(tokens))
+		return (handle_pipeline_execution(tokens, shell));
+	args = prepare_cmd_args(tokens, shell);
+	if (!args)
 		return (127);
-	}
+	if (setup_redirection(tokens, args, shell) == -1)
+		return (1);
 	if (is_builtin(args[0]))
 	{
 		if (ft_strcmp(args[0], "exit") == 0)
-		{
-			status = execute_exit(args, shell);
-			return (status);
-		}
+			return (execute_exit(args, shell));
 		else
 			status = handle_other_builtins(args, shell);
 	}
 	else
 		status = handle_external_command(args, shell);
-	if (args)
-		free_args_array(args);
+	free_args_array(args);
 	shell->exit_status = status;
 	return (status);
 }
